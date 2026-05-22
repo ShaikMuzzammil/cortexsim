2a892646ee8634348cae0bde268fe475e274441a
ShaikMuzzammil <muzzammil160806@gmail.com>
2026-05-22 16:53:27 +0530

    fix NodePalette type error

--- components/Builder/NodePalette.tsx
+++ components/Builder/NodePalette.tsx
@@ -25,7 +25,7 @@ const nodeTypes = [
 ];
 
 export default function NodePalette() {
-  const onDragStart = (event: React.DragEvent<<HTMLDivElement>, nodeType: string) => {
+  const onDragStart = (event: any, nodeType: string) => {
     event.dataTransfer.setData("application/reactflow", nodeType);
     event.dataTransfer.effectAllowed = "move";
   };
@@ -44,7 +44,7 @@ export default function NodePalette() {
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: index * 0.1 }}
             draggable
-            onDragStart={(e: React.DragEvent<<HTMLDivElement>) => onDragStart(e, node.type)}
+            onDragStart={(e: any) => onDragStart(e, node.type)}
             className="flex items-center gap-3 p-3 rounded-lg bg-white/5 hover:bg-white/10 border border-transparent hover:border-neon/20 cursor-grab active:cursor-grabbing transition-all group"
           >
             <GripVertical className="w-3 h-3 text-lavenderGray opacity-0 group-hover:opacity-100" />
